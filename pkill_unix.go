5c6a04d2de4f645ed38188891ad05f36379745f2
Sergei Krivosheenko <serkyron@gmail.com>
2021-03-01 21:28:27 +0300

    search process in unix by full command name

--- pkill_unix.go
+++ pkill_unix.go
@@ -8,9 +8,9 @@ import (
 )
 
 // Pkill will kill a process based on it's name, and will output a string output from the linux pkill command, and also return any errors that occur
-func Pkill(name string) (output string, err error) {
+func Pkill(name string,) (output string, err error) {
 	name = strings.TrimSuffix(name, ".exe")
-	bytes, err := exec.Command("pkill", name).Output()
+	bytes, err := exec.Command("pkill", "-f", name).Output()
 	output = string(bytes)
 	return
 }
